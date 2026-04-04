f0892ab9bda4968adeb0095fb20197db4365d166
maitoyamada09 <maito0399bis1@gmail.com>
2026-04-04 14:23:53 +0900

    Initial Commit

---
+++ DUGOUT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DUGOUT
+//
+//  Created by 山田舞刀 on 2026/04/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DUGOUT/DUGOUTApp.swift
@@ -0,0 +1,17 @@
+//
+//  DUGOUTApp.swift
+//  DUGOUT
+//
+//  Created by 山田舞刀 on 2026/04/04.
+//
+
+import SwiftUI
+
+@main
+struct DUGOUTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
